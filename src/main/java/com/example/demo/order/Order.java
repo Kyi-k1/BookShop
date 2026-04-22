4f7bedda8490fe3ede981c94b293faf25419b65c
User <User@DESKTOP-UFLFNBP>
2026-04-22 10:11:09 +0630

    first commit

--- src/main/java/com/example/demo/order/Order.java
+++ src/main/java/com/example/demo/order/Order.java
@@ -1,5 +1,21 @@
 package com.example.demo.order;
 
-public class Order {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name="book_order")
+public class Order {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
 }
